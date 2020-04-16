3957f91d6e95a1bb1cef0a8dbd92814b7a05f22d
gennadyzaytsev <gennadyza@papayaglobal.com>
2020-04-16 14:30:39 +0300

    added default dynamic form creation template

--- src/main/java/com/papaya/dynamicdatamanagement/form/service/port/in/FormService.java
+++ src/main/java/com/papaya/dynamicdatamanagement/form/service/port/in/FormService.java
@@ -1,4 +1,12 @@
 package com.papaya.dynamicdatamanagement.form.service.port.in;
 
+import com.papaya.dynamicdatamanagement.form.elements.main.Form;
+import com.papaya.dynamicdatamanagement.form.elements.main.FormType;
+
+import java.util.List;
+
 public interface FormService {
+    List<FormType> getAllAvailableFormCreationTypes();
+    List<FormType> getAllAvailableFormTemplatesTypes();
+    List<Form> getAllAvailableForms();
 }
